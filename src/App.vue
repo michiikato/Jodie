aa6db28d55d08841bc2798e49ff91db88858bb41
michiikato <michikokato24@gmail.com>
2025-02-23 08:44:02 +0800

    Jeia Page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Include the SurpriseComponent -->
+    <SurpriseComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import the SurpriseComponent
+import SurpriseComponent from './components/SurpriseComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SurpriseComponent // Register the component here
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Global styles can be added here */
 </style>
